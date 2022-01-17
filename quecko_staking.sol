9ec73b03354f5453724accd1b2e297b118d6914c
Abdul Mueed <62989197+mueed98@users.noreply.github.com>
2022-01-17 18:32:27 +0500

    Update quecko_staking.sol

--- quecko_staking.sol
+++ quecko_staking.sol
@@ -14,17 +14,18 @@ contract StakingToken is ERC20 {
         uint256 starttime; 
     }
 
-
     mapping(address => user) private user_list;
-    mapping(address => uint256) internal stakes;
-    mapping(address => uint256) internal rewards;
-
 
     constructor( ) ERC20("Demo", "DEM")  public
     { 
         uint256 _supply = 100;
         _mint(msg.sender, _supply);
         transfer(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2 , 50 );
+        createStake(50, 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
+        //removeStake(50);
+        withdrawReward();
+
+
     }
 
     // ---------- STAKES ----------
@@ -36,7 +37,7 @@ contract StakingToken is ERC20 {
     function createStake(uint256 _stake, address _referer) public {
         _burn(msg.sender, _stake);
 
-        user_list[msg.sender].accumulatedReward += calculateReward(msg.sender) ;
+        //user_list[msg.sender].accumulatedReward += calculateReward(msg.sender) ;
         user_list[msg.sender].starttime = block.timestamp;
 
         user_list[msg.sender].stakedAmount += _stake;
@@ -49,9 +50,13 @@ contract StakingToken is ERC20 {
      * @param _stake The size of the stake to be removed.
      */
     function removeStake(uint256 _stake) public {
+        require ( stakeOf(msg.sender) > 0 , "Nothing staked" ) ;
         require( (user_list[msg.sender].stakedAmount - _stake) >= 0 , "Cant remove more than stake");
 
         uint256 w_reward = user_list[msg.sender].accumulatedReward + calculateReward(msg.sender);
+        w_reward = distributeReward(w_reward);
+        
+
         
         user_list[msg.sender].stakedAmount -= _stake;
         user_list[msg.sender].accumulatedReward = 0;
@@ -73,16 +78,17 @@ contract StakingToken is ERC20 {
     
     function calculateReward (address _stakeholder) view internal returns (uint256){
             //return (((block.timestamp - user_list[_stakeholder].starttime)/ 24 hours ) ) * 10 * user_list[_stakeholder].stakedAmount;  
-            return  1 * user_list[_stakeholder].stakedAmount;   
+            return 10;   
 
        }
 
     function distributeReward (uint256 w_reward) internal returns(uint256) {
-        if ( user_list[msg.sender].referer != address(0)) {
-            _mint ( user_list[msg.sender].referer , w_reward * 2 / 100 );
-
-            if  ( user_list[ user_list[msg.sender].referer ].referer != address(0) ){
-                _mint ( user_list[msg.sender].referer , w_reward * 1 / 100 );
+        address t_ref = user_list[msg.sender].referer ; 
+        if ( t_ref != address(0)) {
+            _mint ( t_ref , w_reward * 2 / 100 );
+            t_ref = user_list[ t_ref ].referer ;
+            if  ( t_ref != address(0) ){
+                _mint ( t_ref , w_reward * 1 / 100 );
             }
             else
                 return w_reward*98/100;
@@ -98,6 +104,7 @@ contract StakingToken is ERC20 {
 
     function withdrawReward() public{
         uint256 w_reward = user_list[msg.sender].accumulatedReward + calculateReward(msg.sender);
+        w_reward = distributeReward(w_reward);
         require ( w_reward > 0 , "No reward to withdraw" ) ;
 
         user_list[msg.sender].accumulatedReward = 0;
