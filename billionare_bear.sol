04783379c0c19e15e25c0f73796f26b7cd3054a3
Abdul Mueed <62989197+mueed98@users.noreply.github.com>
2022-01-27 18:38:30 +0500

    Update billionare_bear.sol

--- billionare_bear.sol
+++ billionare_bear.sol
@@ -15,23 +15,23 @@ contract BearNFT is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
     uint256 public maxSupply; // max NFT can be minted
     uint256 private maxMintsPerWallet; // max mint an address can do
     uint256 private mintsDuringWhiteList; // allowed minting during whitelist phase
-
     uint256 public mintPrice; // initail price to mint NFT
     uint256 public whitelist_mintPrice; // initail price to mint NFT
+    uint256 private reserveCounter;
+    uint256[] private mintedNFTids; // keeps ids which are minted
     
     mapping (bytes32 => bool ) private NFT_hashes ; // checks URI uniqueness
     mapping (uint256 => bearStruct) public bearList ; // List of NFT data, pass tokenID of NFT as @param
     mapping (address => bool ) private isWhiteList; // List of whitelisted addresses
-    mapping (address => uint256 ) private mintsPerWallet; 
+    mapping (address => uint256[] ) private mintsPerWallet; 
     mapping (uint256 => string) private _tokenURIs;   // To save the actual token URIs
 
     struct bearStruct {
-        uint256 id;
-        string uri;
-        uint price;
         address owner;
         uint256 mintTime;
-        bool preSale;
+        uint256 id;
+        string uri;
+        
     }
     event TokenMinted( uint256 indexed tokenId, address owner);
     event priceSetEvent ( uint ID , bool price_is_set );
@@ -48,11 +48,15 @@ contract BearNFT is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
 
         setMaxSupply( 8888 );
         setPreSaleStatus ( true ) ;
-        addWhiteList ( msg.sender );
         setMintPrice(200000000000000000, 400000000000000000); // 0.2 eth and 0.4 eth
         setPreSaleStatus(true);
         set_maxMintsPerWallet(10);
         set_mintsDuringWhiteList(1000);
+        setReservedCount( 20 );
+
+        address[] memory temp = new address[](1);
+        temp[0] = msg.sender;
+        addWhiteList ( temp );
 
      }
 
@@ -62,6 +66,10 @@ contract BearNFT is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
 
     function getAdmin ()  external view onlyOwner returns (address) {
        return admin ;
+    }
+
+    function setReservedCount(uint256 _reserveCounter) public onlyAdmin{
+        reserveCounter = _reserveCounter;
     }
 
     function setMaxSupply(uint256 _maxSupply) public onlyAdmin{
@@ -94,18 +102,76 @@ contract BearNFT is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
 
 
 
-    function addWhiteList(address id) public onlyAdmin {
-        isWhiteList[id] = true ;
+    function addWhiteList(address[] memory id) public onlyAdmin {
+        for(uint256 i=0; i< id.length ; i++)
+            isWhiteList[id[i]] = true ;
     }
-    function removeWhiteList(address id) public onlyAdmin {
-        isWhiteList[id] = false ;
+    
+    function removeWhiteList(address[] memory id) public onlyAdmin {
+        for(uint256 i=0; i< id.length ; i++)
+            isWhiteList[id[i]] = false ;
     }
+
     function sendContractBalance(address _to) public onlyAdmin {
         (bool sent,) = _to.call{value: address(this).balance }("");
         require(sent == true, "Payment unsuccessful");
         emit payment_Sent(sent);
     }
     
+
+    function reserveMint(string [] memory tokenUriList) external onlyAdmin{
+        require( tokenUriList.length <= reserveCounter, "Can't mint more than reserved" ) ;
+        
+        for (uint256 i=0; i<tokenUriList.length; i++){
+            require(isUniqueNFT(bytes(tokenUriList[i])) == true, "NFT not unique");
+            NFT_hashes[keccak256(bytes(tokenUriList[i]))] = true;
+
+            uint256 tokenId  = safeMint(msg.sender, tokenUriList[i]);
+            
+            mintsPerWallet[msg.sender].push(tokenId) ;
+            reserveCounter -= 1 ;
+            mintedNFTids.push(tokenId);
+            bearList[tokenId].owner = msg.sender;
+            bearList[tokenId].id = tokenId;
+            bearList[tokenId].uri = tokenUriList[i] ;
+            bearList[tokenId].mintTime = block.timestamp; 
+
+        }
+    }
+
+    function batchMint( string[] memory tokenUriList) public payable {
+        require( (mintedNFTids.length + tokenUriList.length) < maxSupply , "Can't mint more than total Supply" );
+        require( (mintsPerWallet[msg.sender].length + tokenUriList.length) <= maxMintsPerWallet, "Can't mint more than maxMintsPerWallet" );
+        if ( preSaleIsON = true )
+            require( (mintedNFTids.length + tokenUriList.length) < mintsDuringWhiteList , "Can't mint more than mintsDuringWhiteList" );
+        
+        uint256 fee = mintPrice;
+        if ( isWhiteList[msg.sender] == true)
+            fee = whitelist_mintPrice;
+
+        (bool sent,) = admin.call{value: fee}("");
+        require(sent == true, "Payment to Admin unsuccessful");
+        emit payment_Sent(sent);        
+
+        for (uint256 i=0; i<tokenUriList.length; i++){
+            require(isUniqueNFT(bytes(tokenUriList[i])) == true, "NFT not unique");
+            NFT_hashes[keccak256(bytes(tokenUriList[i]))] = true;
+
+            uint256 tokenId  = safeMint(msg.sender, tokenUriList[i]);
+            mintedNFTids.push(tokenId);
+            mintsPerWallet[msg.sender].push(tokenId) ;
+            bearList[tokenId].owner = msg.sender;
+            bearList[tokenId].id = tokenId;
+            bearList[tokenId].uri = tokenUriList[i] ;
+            bearList[tokenId].mintTime = block.timestamp; 
+
+        }
+
+
+    }
+
+
+
 
     function setTokensURI(uint256 [] memory tokenIds, string [] memory tokenUris) external onlyAdmin{
         require (tokenIds.length == tokenUris.length, "Length of Uri and Ids must be same");
@@ -148,7 +214,7 @@ contract BearNFT is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
     *
     * @param _tokenURI  -- URI to be checked for uniqueness
     */
-    function isUniqueSVG_by_Hash(bytes memory _tokenURI ) private view returns (bool) {
+    function isUniqueNFT(bytes memory _tokenURI ) private view returns (bool) {
 
         if (NFT_hashes[keccak256(_tokenURI)] == true){
             return false;
