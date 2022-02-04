cf74f7b02eb588a715b22483db759bd5be4531de
Abdul Mueed <62989197+mueed98@users.noreply.github.com>
2022-02-04 17:29:38 +0500

    Update royalty_contract.sol

--- royalty_contract.sol
+++ royalty_contract.sol
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "hardhat/console.sol";
 
 
 contract main is Ownable {
 
-    mapping(address => bool ) private royalty_getters_map;
+    mapping(address => bool ) private map_royalty_getters;
     address[] public royalty_getters;
 
     event payment_Sent(bool payment_Sent);
@@ -13,11 +15,47 @@ contract main is Ownable {
     constructor() {
 
     }
+    
+    function test() payable external {
+
+        addRoyaltyGetter(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
+        addRoyaltyGetter(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
+        addRoyaltyGetter(0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db);
+        addRoyaltyGetter(0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db);
+
+        console.log( "Contact Balance : ", checkBalance() );
+        trigger();
+    }
 
     function addRoyaltyGetter(address _royalty_getter ) public onlyOwner {
-        if ( royalty_getters_map [ _royalty_getter ] == false ){
+        if ( map_royalty_getters [ _royalty_getter ] == false ){
             royalty_getters.push(_royalty_getter);
-            royalty_getters_map [ _royalty_getter ] = true;
+            map_royalty_getters [ _royalty_getter ] = true;
+
+            console.log("Adding to Royalty Getters :", _royalty_getter );
+        }
+
+    }
+
+    function giveEther() payable public {
+
+    }
+
+    function checkBalance() public view returns(uint256) {
+            return address(this).balance;
+    }
+
+    function trigger() public {
+        uint256 fee = address(this).balance / royalty_getters.length ;
+        console.log( "Share per Person (approx) : ", fee / 1 ether , " ETH"  );
+
+        for (uint256 i=0; i<royalty_getters.length; i++) {
+        console.log( "Sending Eth to : ", royalty_getters[i]  );
+        Address.sendValue(payable(royalty_getters[i]), fee);
+
+        // (bool sent,) = royalty_getters[i].call{value: fee }("");
+        // require(sent == true, "Payment unsuccessful");
+        // emit payment_Sent(sent);
         }
 
     }
