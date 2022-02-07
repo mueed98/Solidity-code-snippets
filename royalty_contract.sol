41f8dfb826d2ae3a1fac44fcbe2d478db2d6e692
Abdul Mueed <62989197+mueed98@users.noreply.github.com>
2022-02-07 14:36:29 +0500

    Update royalty_contract.sol

--- royalty_contract.sol
+++ royalty_contract.sol
@@ -2,10 +2,11 @@
 pragma solidity 0.8.7;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "hardhat/console.sol";
 
 
-contract main is Ownable {
+contract main is Ownable,  ReentrancyGuard {
 
     mapping(address => bool ) private map_royalty_getters;
     address[] public royalty_getters;
@@ -21,31 +22,26 @@ contract main is Ownable {
         addRoyaltyGetter(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
         addRoyaltyGetter(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
         addRoyaltyGetter(0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db);
-        addRoyaltyGetter(0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db);
 
-        console.log( "Contact Balance : ", checkBalance() );
+        console.log( "Contact Balance : ", ( checkBalance() / 1 ether ) , " ETH");
+        
+        trigger();
         trigger();
     }
 
     function addRoyaltyGetter(address _royalty_getter ) public onlyOwner {
-        if ( map_royalty_getters [ _royalty_getter ] == false ){
-            royalty_getters.push(_royalty_getter);
-            map_royalty_getters [ _royalty_getter ] = true;
+        require(map_royalty_getters [ _royalty_getter ] == false , "Address Already Added") ;
 
-            console.log("Adding to Royalty Getters :", _royalty_getter );
-        }
-
-    }
-
-    function giveEther() payable public {
-
+        map_royalty_getters [ _royalty_getter ] = true;
+        royalty_getters.push(_royalty_getter);
+        console.log("Adding to Royalty Getters :", _royalty_getter );
     }
 
     function checkBalance() public view returns(uint256) {
             return address(this).balance;
     }
 
-    function trigger() public {
+    function trigger() public nonReentrant(){
         uint256 fee = address(this).balance / royalty_getters.length ;
         console.log( "Share per Person (approx) : ", fee / 1 ether , " ETH"  );
 
@@ -53,9 +49,6 @@ contract main is Ownable {
         console.log( "Sending Eth to : ", royalty_getters[i]  );
         Address.sendValue(payable(royalty_getters[i]), fee);
 
-        // (bool sent,) = royalty_getters[i].call{value: fee }("");
-        // require(sent == true, "Payment unsuccessful");
-        // emit payment_Sent(sent);
         }
 
     }
